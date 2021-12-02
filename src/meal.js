a8846fbdbbea3c63e35405b77cd6131a57a44e83
Francesca McConnell <fmcconnell59@gmail.com>
2021-12-01 18:59:46 -0700

    Add Meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
 }
 
+
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type
+  }
+  return menuItem;
+}
+
+function addIngredients(ingredient, array) {
+  if (array.includes(ingredient) === false) array.push(ingredient);
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(price) {
+  var percent = .1;
+  return price - (price * percent);
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
