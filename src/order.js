d51cd25572f7ab984f42f676425f716e8bff2f7d
Francesca McConnell <fmcconnell59@gmail.com>
2021-12-03 06:42:54 -0700

    Add order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,34 @@
+function takeOrder(order, array) {
+  if (array.length < 3) array.push(order);
+}
+
+function refundOrder(number, array) {
+  for (var i = 0; i < array.length; i++) {
+    if (number === array[i].orderNumber) {
+      array.splice(i, 1);
+    }
+  }
+}
+
+function listItems(array) {
+  var itemNames = [];
+  for (var i = 0; i < array.length; i++) {
+    if (i != 0) {
+      itemNames.push(" " + array[i].item);
+    } else {
+      itemNames.push(array[i].item)
+    }
+  }
+
+return itemNames.toString();
+
+}
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
